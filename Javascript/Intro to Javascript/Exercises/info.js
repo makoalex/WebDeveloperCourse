9c1baeb0dff52468c79b12a9623274efb720328a
Alexandra Macovei <macovei.a2@gmail.com>
2020-09-03 12:06:19 +0200

    first javascript exercise

---
+++ Javascript/Intro to Javascript/Exercises/info.js
@@ -0,0 +1,5 @@
+var user_name = prompt("Hey what  is your name? ");
+var user_l_name = prompt("And  what is your last name?");
+var user_age = prompt("Now your age ");
+console.log('your full  name is: '+ user_name +""+ user_l_name);
+console.log("your age is: "+ user_age);
