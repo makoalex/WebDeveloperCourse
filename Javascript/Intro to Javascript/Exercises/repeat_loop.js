f9df659e336d7f73bf50c81de67d160d363ff95a
Alexandra Macovei <macovei.a2@gmail.com>
2020-09-05 11:25:21 +0200

    adding indexof() methond to the loop

--- Javascript/Intro to Javascript/Exercises/repeat_loop.js
+++ Javascript/Intro to Javascript/Exercises/repeat_loop.js
@@ -2,4 +2,7 @@ var question = prompt("we've been driving for long.. Are we there yet??!!");
 while(question !== 'yes' && question !== 'yeah' && question.includes('yes', 'yeah')===-1) {
   var question = prompt("Are we there now?");
 }
-alert("YAy, Finally")
+alert("Yay, Finally")
+
+
+// Indexof() method gives  the first position of an occurring element, and it returns -1 if the element is not found
