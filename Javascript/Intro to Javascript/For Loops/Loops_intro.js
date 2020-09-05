261c90f679c9c78870a607327951eb2eb35c6dbc
Alexandra Macovei <macovei.a2@gmail.com>
2020-09-05 12:04:09 +0200

    basic loop intro with Javascript

---
+++ Javascript/Intro to Javascript/For Loops/Loops_intro.js
@@ -0,0 +1,15 @@
+// Loops syntax
+// for(initialize, condition, step){
+//code in here}
+
+// counting from 1 to 6 with for loop
+for(count = 1; count <= 6; count++){
+  console.log(count);
+}
+
+// printing each character in a string
+
+var name = "mako";
+for (var i =0; i < name.length;i++){
+  console.log(name[i])
+}
