ea029b3838ef4a5c3368c342047a6a40529e48a5
Alexandra Macovei <macovei.a2@gmail.com>
2020-09-25 11:13:36 +0200

    change input

--- Javascript/DOM/Advanced DOM/Score keeping project/Score_keeper.js
+++ Javascript/DOM/Advanced DOM/Score keeping project/Score_keeper.js
@@ -1,19 +1,19 @@
-//start by selecting player one and player two button, and incrementing the score
-
 var player1 = document.getElementById('one');
 var player2 = document.getElementById('two');
 var first_score = document.getElementById('first_score');
 var second_score = document.querySelector('#second_score');
 var resetScore = document.getElementById('reset');
+var inputNumber = document.querySelector('input');
 var player1_score = 0;
 var player2_score = 0;
 var gameOver = true;
-var number_plays = Number(prompt('How many games would you like to play?'));
+var number_plays = 5;
 
 function player_one() {
   player1.addEventListener('click', function () {
     if (gameOver) {
       player1_score++
+      console.log(player1_score, number_plays);
       if (player1_score === number_plays) {
         first_score.classList.add('winner');
         alert('player one won');
@@ -51,12 +51,21 @@ function reset() {
     second_score.classList.remove('winner');
     gameOver= true;
   });
-  return resetScore;
+  return resetScore
 }
 
-
+function input(){
+  choseNumber = document.querySelector('p span');
+  inputNumber.addEventListener('input', addValue);
+  console.log(inputNumber);
+  function addValue(e){
+    choseNumber.textContent = e.target.value;
+    number_plays = Number(inputNumber.value ); 
+  }
+}
 
 
 player_one()
 player_two()
 reset()
+input()
