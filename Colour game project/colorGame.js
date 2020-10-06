582a7c1218783c43e7a766d9811ff28414588254
Alexandra Macovei <macovei.a2@gmail.com>
2020-10-06 10:26:37 +0200

    writing code for selecting colours and adding functionality for the easy and hard levels

--- Colour game project/colorGame.js
+++ Colour game project/colorGame.js
@@ -1,21 +1,74 @@
-var colours = generateColoursArray(6);
+var num = 6
+var colours = generateColoursArray(num);
 var square = document.querySelectorAll('.square');
 var title_color = document.querySelector('span');
 var picked_colour = random_colour_pick();
 title_color.textContent = picked_colour;
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
+resetButton = document.getElementById('reset');
+var body = document.querySelector('body');
+var easy = document.getElementById('easy');
+var hard = document.getElementById('hard');
 
-
-for (var i = 0; i < square.length; i++) {
-  square[i].style.backgroundColor = colours[i];
+function pick_squareColour() {
+  for (var i = 0; i < square.length; i++) {
+    square[i].style.backgroundColor = colours[i];
+  };
 }
 
-function generateColoursArray(num){
+function easyLevel() {
+  easy.addEventListener('click', function () {
+    easy.classList.add('selected');
+    hard.classList.remove('selected');
+    num = 3;
+    colours = generateColoursArray(num);
+    picked_colour = random_colour_pick()
+    title_color.textContent = picked_colour;
+    for (var i = 0; i < square.length; i++) {
+      if (colours[i]) {
+        square[i].style.backgroundColor = colours[i];
+      } else {
+        square[i].style.display = 'none';
+      }
+    }
+  });
+}
+
+function hardLevel() {
+  hard.addEventListener('click', function () {
+    hard.classList.add('selected');
+    easy.classList.remove('selected');
+    num = 6
+    colours = generateColoursArray(num)
+    for (var i = 0; i < square.length; i++) {
+      square[i].style.display = 'block';
+    }
+  });
+}
+
+
+function New_colours() {
+  resetButton.addEventListener('click', function () {
+    //generate all new colour
+    colours = generateColoursArray(num);
+    //pick a new random colour from the array
+    picked_colour = random_colour_pick();
+    // change the colour display to match the picked square
+    title_color.textContent = picked_colour;
+    //change colours of the squares
+    pick_squareColour();
+    h1.style.backgroundColor = body.style.backgroundColor;
+    resetButton.textContent = 'New Colours';
+  })
+}
+
+
+function generateColoursArray(num) {
   //create and array
   var arr = []
   //repeat num times
-  for (var i = 0; i < num; i++){
+  for (var i = 0; i < num; i++) {
     //get random colour and push into arr
     arr.push(randomColour())
   }
@@ -23,14 +76,14 @@ function generateColoursArray(num){
   return arr;
 }
 
-function randomColour(){
+function randomColour() {
   //pick a red from 0-255
-  var red = Math.floor(Math.random()* 256);
+  var red = Math.floor(Math.random() * 256);
   //pick a green from 0-255
-  var  green = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
   //pick a blue from 0-255
   var blue = Math.floor(Math.random() * 256);
-  return 'rgb('+ red+', ' +green+', '+ blue+')'
+  return 'rgb(' + red + ', ' + green + ', ' + blue + ')'
 }
 
 
@@ -54,6 +107,7 @@ function rightAnswerColour(colour) {
     square[i].style.backgroundColor = colour;
   }
   h1.style.backgroundColor = colour;
+  resetButton.textContent = 'Play Again';
 }
 
 function random_colour_pick() {
@@ -61,4 +115,9 @@ function random_colour_pick() {
   var randomColour = Math.floor(Math.random() * colours.length);
   return colours[randomColour];
 }
+
+pick_squareColour()
 pickColour()
+New_colours()
+easyLevel()
+hardLevel()
