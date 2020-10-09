bcf1d82d126fd5e598cf899a177b0de29f1d1f8c
Alexandra Macovei <macovei.a2@gmail.com>
2020-10-09 13:59:49 +0200

    selecting and style exercise

---
+++ Jquery/Jquery_exercise/Jquery_exercise.js
@@ -0,0 +1,12 @@
+var divs = $('div').css('background', 'purple');
+var high = $('.highlight'). css({
+  width: '200px',
+  fontSize: '40px'
+});
+var id = $('#third'). css({
+  border: '2px solid orange'
+});
+
+var style4 = $('div:nth-of-type(1)').css({
+  color: 'pink'
+});
