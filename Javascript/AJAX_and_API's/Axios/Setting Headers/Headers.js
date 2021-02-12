abb9ce2b111f98c04fc2a37a3ef6a0873044bfe8
Alexandra Macovei <macovei.a2@gmail.com>
2021-02-12 11:28:24 +0100

    Adding headers with Axios, and making a little joke showing prog

---
+++ Javascript/AJAX_and_API's/Axios/Setting Headers/Headers.js
@@ -0,0 +1,25 @@
+// in order to add headers, we have to configure the headers as another parameter
+
+const firstHeader = async() =>{
+  try{
+    const headerconfig= {headers: {Accept: 'application/json'}}
+    const response = await axios.get('https://icanhazdadjoke.com/', headerconfig);
+    console.log(response.data.joke)
+    newJoke(response.data.joke)
+  }catch(e){
+    console.log('its an error', e )
+    newJoke('No jokes to show', e)
+  }
+  }
+
+
+
+const buttonjoke = document.querySelector('#joke');
+buttonjoke.addEventListener('click', firstHeader)
+
+ function newJoke(data){
+  const list= document.querySelector('#list');
+  const newLi = document.createElement('li')
+  newLi.append(data)
+  list.appendChild(newLi)
+ }
