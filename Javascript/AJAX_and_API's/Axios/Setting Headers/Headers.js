cfbd4a4245687cd814850a85edfc575074fb9958
Alexandra Macovei <macovei.a2@gmail.com>
2021-02-12 11:57:54 +0100

    Added an async version of the same functions

--- Javascript/AJAX_and_API's/Axios/Setting Headers/Headers.js
+++ Javascript/AJAX_and_API's/Axios/Setting Headers/Headers.js
@@ -23,3 +23,26 @@ buttonjoke.addEventListener('click', firstHeader)
   newLi.append(data)
   list.appendChild(newLi)
  }
+
+// const firstHeader = async() =>{
+//   try{
+//     const headerconfig= {headers: {Accept: 'application/json'}}
+//     const response = await axios.get('https://icanhazdadjoke.com/', headerconfig);
+//     console.log(response.data.joke)
+//     return response.data.joke
+//   }catch(e){
+//     console.log('its an error', e )
+    
+//   }
+//   }
+
+//   const newJoke = async() =>{
+//     const joke = await firstHeader();
+//       const list= document.querySelector('#list');
+//       const newLi = document.createElement('li')
+//       newLi.append(joke)
+//       list.appendChild(newLi)
+//      }
+     
+// const buttonjoke = document.querySelector('#joke');
+// buttonjoke.addEventListener('click', newJoke)
