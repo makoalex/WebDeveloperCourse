e58d8c873774ce3f0fe59ed683f197bbb1a62e64
Alexandra Macovei <macovei.a2@gmail.com>
2021-03-02 11:38:45 +0100

    More Practice with Classes in Javascript

--- Prototypes classes and OOP/Javascript Classes/Classes.js
+++ Prototypes classes and OOP/Javascript Classes/Classes.js
@@ -18,6 +18,7 @@ class Colour{
     this.r = r;
     this.g = g;
     this.b = b;
+    this.calcHSL();
   }
   makeRGB(){
     const {r,g,b} =this;
@@ -29,5 +30,62 @@ class Colour{
   rgba(a = 1){
     return `rgba(${this.makeRGB()}, ${a})`
   }
+  toHex(){
+    const {r,g,b} = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8)+b).toString(16).slice(1)
+  }
+	calcHSL() {
+		let { r, g, b } = this;
+    // TAKEN OFF STACKOVERFLOW
+		// Make r, g, and b fractions of 1
+		r /= 255;
+		g /= 255;
+		b /= 255;
+
+		// Find greatest and smallest channel values
+		let cmin = Math.min(r, g, b),
+			cmax = Math.max(r, g, b),
+			delta = cmax - cmin,
+			h = 0,
+			s = 0,
+			l = 0;
+		if (delta == 0) h = 0;
+		else if (cmax == r)
+			// Red is max
+			h = ((g - b) / delta) % 6;
+		else if (cmax == g)
+			// Green is max
+			h = (b - r) / delta + 2;
+		else
+			// Blue is max
+			h = (r - g) / delta + 4;
+
+		h = Math.round(h * 60);
+
+		// Make negative hues positive behind 360°
+		if (h < 0) h += 360;
+		// Calculate lightness
+		l = (cmax + cmin) / 2;
+
+		// Calculate saturation
+		s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+		// Multiply l and s by 100
+		s = +(s * 100).toFixed(1);
+		l = +(l * 100).toFixed(1);
+		this.h = h;
+		this.s = s;
+		this.l = l;
+	}
+  toHsl(){
+    const {h,s, l} = this;
+    return `hsl(${h}, ${s}%, ${l}%)`
+  }
+  oppositeHsl(){
+    const {h,s,l} = this;
+    const oppositeHsl= (h+180) %360;
+    return `hsl(${oppositeHsl}, ${s}%, ${l}%)`
+  }
+  
 }
 const mossGreen = new Colour(151, 166, 106);
