b790abcf040b5a53ae6d3fd6a2738e815077212a
Alexandra Macovei <macovei.a2@gmail.com>
2021-03-11 16:18:21 +0100

    learning to work with module exports

---
+++ Node/Module_NPM/Working with module exports/math.js
@@ -0,0 +1,13 @@
+module.exports.square = x=> x*x;
+module.exports.add = (a,b) => a+b;
+module.exports.multiplication = (x,z) => x*z;
+
+// after we have defined the functions:
+// module.exports.add = add;
+// module.exports.square = square;
+// module.exports.multiplication = multiplication;
+
+// // or iit is more common to write as long as we don't redefine exports
+// exports.add = add;
+// exports.square = square;
+// exports.multiplication = multiplication;
