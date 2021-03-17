53e37a4d73561ef22356b0ba8b543db5aab315d1
Alexandra Macovei <macovei.a2@gmail.com>
2021-03-17 12:11:23 +0100

    generating a request and response using the REG and RES objects

--- Express_intro/firstApp/index.js
+++ Express_intro/firstApp/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 // everytime  a requst hits our server we print this out
-app.use(() => {
+// in order to get a response we need to pass 2 parameters to USE() request and response 
+// they're objects made by Express( parses all the information into an object, by parsing the incoming HTTP information => request)
+app.use((req, res) => {
   console.log('We got a new request!')
+  res.send('HELLO THERE STRANGER')
 })
 app.listen(3000, () =>{
   console.log('Listening on port 3000!')
