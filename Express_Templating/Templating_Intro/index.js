8a5d7d74124019f17dcb3039aa52caf0080d7517
Alexandra Macovei <macovei.a2@gmail.com>
2021-03-22 12:46:17 +0100

    Adding PATH.JOIN in order to access the vies dorectory for any other directory

--- Express_Templating/Templating_Intro/index.js
+++ Express_Templating/Templating_Intro/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { dirname } = require('path');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs')
+// setting views directory in order ot be able to work from different directories
+const path = require('path');
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) =>{
   // res.send('hi')
