15aa8cb362587150f886a2c4d35cc14be8444ddc
Alexandra Macovei <macovei.a2@gmail.com>
2021-04-08 10:58:37 +0200

    parsing the request body

--- RestDemo/index.js
+++ RestDemo/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.get('/tacos', (req, res) =>{
   res.send('GET SEND')
 })
 
 app.post('/update', (req, res) => {
-  res.send('POST REQUEST SUCCESSFUL')
+  console.log(req.body)
+  const {meat, qty } = req.body
+  res.send(`Here is your order update for ${qty} of ${meat}`)
 })
 
 app.listen(port, () =>{
