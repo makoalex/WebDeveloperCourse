5a0b93d3ff50da007a346cf5c450e0c9a69b0523
Alexandra Macovei <macovei.a2@gmail.com>
2021-04-12 10:43:22 +0200

    implementing some RESTful routes for a comments resource

--- RestDemo/index.js
+++ RestDemo/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+const comments =[
+  {
+    user:'mako',
+    comment: 'This film was clearly underrated'
+  },
+
+   {
+      user : 'badass',
+    comment : 'i liked the CGI '
+   },
+
+    {
+      user : 'fred400',
+    comment : "seen it  times already. Can't wait for the sequel"
+}
+];
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'/views'))
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/comments', (req, res)=>{
+  res.render('comments/index', {comments})
+})
 
 app.get('/tacos', (req, res) =>{
   res.send('GET SEND')
